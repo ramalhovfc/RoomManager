a625bebf6005b194ef01e7949cc3c2845b6eaaa2
Francisco <franciscofsantos11@gmail.com>
2017-01-19 13:02:37 +0000

    Checin/out need optimisation

--- main.py
+++ main.py
@@ -1,78 +1,77 @@
-from bottle import Bottle, template, request, response, debug, get, post, static_file, redirect
+from bottle import Bottle, run, template, request, debug, get, post, static_file, redirect
+from google.appengine.ext import ndb
+import json
+import requests
+import random
 import templates
 import requests_toolbelt.adapters.appengine
-import json
-from google.appengine.ext import ndb
-
-from CheckRoom import CheckRoom
-from Room import Room
-from User import User
-from SpaceFetcher import SpaceFetcher
 
 requests_toolbelt.adapters.appengine.monkeypatch()
 
-fenixFetcher = SpaceFetcher("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces")
+class User(ndb.Model):
+	username = ndb.StringProperty()
+	userid = ndb.IntegerProperty()
+	checked_in = ndb.IntegerProperty()
+
+class Room(ndb.Model):
+	roomid = ndb.IntegerProperty()
+	roomname = ndb.StringProperty()
+
+class CheckRoom(ndb.Model):
+	userid = ndb.IntegerProperty(repeated=True)
+	roomid = ndb.IntegerProperty()
 
 debug(True)
 
 # Create the Bottle WSGI application.
 bottle = Bottle()
 
-@bottle.route('/static/<filename>', name='static')
-def server_static(filename):
-	return static_file(filename, root='static')
+@bottle.route('/js/:filename:')
+def send_static(filename):
+    return static_file(filename, root='./js/')
 
+#ecra inicial e login do utilizador
 @bottle.route('/')
 def home():
-	return templates.index
+    return templates.index
 
 @bottle.route('/', method="post")
 def do_login():
 	username = request.forms.get('username')
 	#print ("O teu username e", username)
-	userId = login_user(username)
-
-	if userId < 0: #username already exists
+	userid=login_user(username)
+	if(userid<0): #username already exists
 		return template(templates.temp_failed_login, username=username)
+	elif (userid==0): #admin
+		redirect("/0")
+	elif (userid>0): #regular user with valid username
+		redirect("/user/" +str(userid))
+	#return template(templates.temp_utilizadores, list=utilizadores)
+	
+@bottle.route('/user/<uid:int>') #para identificar o admin
+def user_actions(uid):
+	return template(templates.logged_in, uid=uid)
 
-	elif userId == 0: #admin
-		response.set_cookie("userId", str(userId))
-		redirect("/admin")
-	elif userId > 0: #regular user with valid username
-		response.set_cookie("userId", str(userId))
-		redirect("/user")
-
-@bottle.route('/user')
-def user_actions():
-	userId = request.get_cookie("userId")
-	return template(templates.logged_in, uid = userId, get_url = bottle.get_url)
-
-@bottle.route('/admin')
+@bottle.route('/0') #para identificar o admin
 def list_spaces():
-	spaces = fenixFetcher.getSpaceById()
-	return template(templates.temp_spaces, list = spaces)
+	#apagar esta linha
+	criar_sala()
+	inserir_user_sala()
 
-@bottle.route('/provideRoom/<roomId>/<roomName>', method="post")
-def provideRoom(roomId, roomName):
-	criar_sala(int(roomId), roomName)
+	r = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces')
+	spaces = json.loads(r.text)
+	return template (templates.temp_spaces ,list = spaces)
 
-@bottle.route('/isRoomProvided/<roomId>', method="get")
-def provideRoom(roomId):
-	return str(isRoomProvided(roomId))
-
-@bottle.route('/testing', method="post")
-def testing():
-	print "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-	return str(json.load(request.body))
-
-@bottle.route('/0/space/<id_space>')
+@bottle.route('/0/space/<id_space:int>')
 def buildings(id_space):
-	building = fenixFetcher.getSpaceById(id_space)
+	complete_path = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"+str(id_space)
+	r = requests.get(complete_path)
+	building = json.loads(r.text)
 	if (building["containedSpaces"] != []):
-		return template(templates.temp_spaces, list = building["containedSpaces"], get_url = bottle.get_url)
+		return template (templates.temp_spaces, list = building["containedSpaces"])
 	else:
 		print ("cheguei a uma sala para reservar!")
-		return template (templates.temp_provide, list = building, get_url = bottle.get_url)
+		return template (templates.temp_provide, list = building) 	
 
 @bottle.route('/user/<uid:int>/rooms')
 def show_rooms(uid):
@@ -82,127 +81,199 @@ def show_rooms(uid):
 	id_rooms["id"]=uid
 	print "rooms ---------------------------------------"
 	print rooms
-	return template(templates.temp, list= id_rooms, get_url = bottle.get_url)
+	return template(templates.temp, list= id_rooms)
 
-@bottle.route('/api/checkin', method ="post") #pode ser feito assim ou passando as coisas pelo url
+@bottle.route('/api/checkin', method ="post") 
 def check_in_datase():
-	roomId = request.forms.get('roomId')
-	userId = request.forms.get('uid')
+	#roomid = request.forms.get('roomid')
+	#userid = request.forms.get('uid')
+
+	data = json.load(request.body)
+	roomid = data["roomid"]
+	userid = data["uid"]
 
 	print "Api funciona !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	print userId, roomId
+	print userid, roomid
 	#falta fazer query na base de dados para ver se utilziador ja fez check in
-	key_cr=ndb.Key(CheckRoom, int(roomId))
-	exemplo=key_cr.get()
-	print "===================================================="
-	if exemplo != None:
-		buf = exemplo.userId
-		buf.append(int(userId))
-		exemplo.userId=buf
-		exemplo.put()
-
-	else:
-		checked_room = CheckRoom(roomId = int(roomId), userId = [int(userId)])
-		checked_room.key = ndb.Key(CheckRoom, int(roomId))
-		checked_room.put()
-
-
-	key=ndb.Key(User, int(userId))
+	
+	key=ndb.Key(User, int(userid))
 	user = key.get()
-	user.checked_in = True
-	user.put()
 
-	#user_exemplo = User(username = username, userId = userId, checked_in = True)
+	if (user.checked_in == -1): #O utilizador nao estava logado em nenhuma sala
+		user.checked_in = int(roomid)
+		user.put()
+
+		key_cr=ndb.Key(CheckRoom, int(roomid))
+		exemplo=key_cr.get()
+		if exemplo != None: #ja existiam utilziadores na sala, acrescentar o novo
+			buf = exemplo.userid
+			buf.append(int(userid))
+			exemplo.userid=buf
+			exemplo.put()
+
+		else: #ainda nao existia ninguem na sala
+			checked_room = CheckRoom(roomid = int(roomid), userid = [int(userid)])
+			checked_room.key = ndb.Key(CheckRoom, int(roomid))
+			checked_room.put()
+
+		resposta = json.dumps({'state': 1})
+
+	else: #utilizador estava logado numa sala, primeiro fazer logout e depois voltar a fazer login 
+		resposta = json.dumps({'state': 0})
+
+	return resposta
+
+	#user_exemplo = User(username = username, userid = userid, checked_in = True)
 	#user_exemplo.put()
 
-@bottle.route('api/checkout', method="post")
+@bottle.route('/api/checkout', method="post")
 def check_out_database():
-	roomId = request.forms.get('roomId')
-	userId = request.forms.get('uid')
+	#roomid = request.forms.get('roomid')
+	#userid = request.forms.get('uid')
+	#recebido = json.loads(request.body)
 
-	print "fazer checkout"
+	userid2= json.load(request.body)
+	userid = userid2["uid"]
+	#print recebido
+	print "fazer checkout ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	print userid
+	#key_cr=ndb.Key(CheckRoom, int(roomid))
+	#exemplo=key_cr.get()
 
-	key_cr=ndb.Key(CheckRoom, int(roomId))
-	exemplo=key_cr.get()
+#	if (exemplo!=None): #retirar utilizador da sala e alterar o seu estado para nao checked in
+#		buf = exemplo.userid
+#		buf.remove(userid)
+#		exemplo.userid=buf
+#		exemplo.put()
+	key_u=ndb.Key(User, int(userid))
+	exemplo2=key_u.get()
+	roomid=exemplo2.checked_in
+	if roomid == -1: #verificar se esta logado numa sala
+		resposta = json.dumps({'state': 0})
+	else:
+		exemplo2.checked_in = -1
+		exemplo2.put()
+		resposta = json.dumps({'state': 1})
 
-	if (exemplo!=None):
-		print("none")
-	else: # a fazer depois quando o utilizador nao esta na sala
-		print("dsfasdfsdfas")
+		key_cr=ndb.Key(CheckRoom, int(roomid))
+		exemplo=key_cr.get()
+		buf = exemplo.userid
+		buf.remove(int(userid))
+		exemplo.userid=buf
+		exemplo.put()
 
-def isRoomProvided(roomId):
-	try:
-		roomIdParsed = int(roomId)
-	except:
-		return str(False)
 
-	availableRooms = list_available_rooms()
-	return str(roomId in availableRooms)
+	#resposta = {'state': 1}
+	#resposta2=json.dumps(resposta)
+
+	return resposta
+
+
+#	else: # a fazer depois quando a sala nao esta na base de dados
+#		print("dsfasdfsdfas")
+
+"""@bottle.route('/api/listusers/<id_sala:int>')
+def show_listed_users_api(id_sala):
+	redirect("/listusers/"+int(id_sala))
+"""
+
+@bottle.route('/api/listusers/<id_sala:int>')
+def show_listed_users(id_sala):
+
+	key=ndb.Key(CheckRoom, int(id_sala))
+	room=key.get()
+
+	users = {}
+
+	#print room.userid
+
+	if (room is None):
+		resposta = {'state':0, 'users':users} #Nao esta ninguem logado na sala 
+	else:
+		for userident in room.userid:
+			#print int(userident)
+			key_u=ndb.Key(User, int(userident))
+			user_all=key_u.get()
+			users[int(userident)]=user_all.username
+		resposta = {'state':1, 'users':users} #existem utilizadores logados na sala	
+
+	return  json.dumps(resposta) #nao esta a mudar de pagina com o template, javascript ver
 
 def login_user(username):
-#verifica se o username ja existe
-	max_userId=0
+#verifica se o username ja existe 
+	max_userid=0
 	if (username!="admin"):
 		query=User.query().fetch()
 		print query
 		for utilizador in query:
 			print utilizador
-			if utilizador.userId>max_userId:
-				max_userId=utilizador.userId
+			if utilizador.userid>max_userid:
+				max_userid=utilizador.userid
 			if utilizador.username == username:
 				return -1
-		max_userId=max_userId+1
-		user_exemplo = User(username = username, userId = max_userId, checked_in = False)
-		user_exemplo.key = ndb.Key(User, max_userId)
+		max_userid=max_userid+1
+		user_exemplo = User(username = username, userid = max_userid, checked_in = -1)
+		user_exemplo.key = ndb.Key(User, max_userid)
 		user_exemplo.put()
-		return max_userId
+		return max_userid
 	else:
-		user_exemplo = User(username = username, userId = 0,  checked_in = False)
-		user_exemplo.key = ndb.Key(User, max_userId)
+		user_exemplo = User(username = username, userid = 0,  checked_in = -1)
+		user_exemplo.key = ndb.Key(User, max_userid)
 		user_exemplo.put()
 		return 0
 
 def convert_username(username):
+
 	query=User.query().fetch()
 	for utilizador in query:
 		if username==utilizador.username:
-			return username.userId
+			return username.userid
 
 	return -1
 
 #show available rooms to check_in
 def list_available_rooms():
+
 	query = Room.query().fetch()
 	available_rooms = {}
 	for room in query:
-		available_rooms[room.key]=room.roomName
-		print room.key," ",room.roomName
+		available_rooms[room.roomid]=room.roomname
+		print room.roomid," ",room.roomname	
 
 	return available_rooms
 
 #apagar, funcao de teste
-def criar_sala(roomId, roomName):
-	sala = Room(roomId = roomId, roomName = roomName)
-	sala.saveToCloud()
+def criar_sala():
+	sala_exemplo = Room(roomid = 1234, roomname = "sala 1")
+	sala_exemplo.key = ndb.Key(Room, 1234)
+	sala_exemplo.put()
+
+	sala_exemplo2 = Room(roomid = 567, roomname = "sala 2")
+	sala_exemplo2.key = ndb.Key(Room, 567)
+	sala_exemplo2.put()
+
+	sala_exemplo3 = Room(roomid = 89, roomname = "sala 3")
+	sala_exemplo3.key = ndb.Key(Room, 89)
+	sala_exemplo3.put()
 
 #funcao de teste apagar
 def inserir_user_sala():
-	room = CheckRoom(roomId = 89, userId =[1,2,3])
+	room = CheckRoom(roomid = 89, userid =[1,2,3])
 	room.put()
 
-	"""room2 = CheckRoom(roomId = 89, userId = 2)
+	"""room2 = CheckRoom(roomid = 89, userid = 2)
 	room2.put()
 
-	room3 = CheckRoom(roomId = 89, userId = 3)
+	room3 = CheckRoom(roomid = 89, userid = 3)
 	room3.put()"""
 
-	room4 = CheckRoom(roomId = 1234, userId = [1])
+	room4 = CheckRoom(roomid = 1234, userid = [1])
 	room4.put()
 
-	room5 = CheckRoom(roomId = 567, userId = [1])
+	room5 = CheckRoom(roomid = 567, userid = [1])
 	room5.put()
 
-	#room6 = CheckRoom(roomId = 1234, userId = 1)
+	#room6 = CheckRoom(roomid = 1234, userid = 1)
 	#room6.put()
 
 
--- templates.py
+++ templates.py
@@ -1,3 +1,22 @@
+#from bottle import Bottle, run, template, request, debug, get, post, static_file
+#import requests
+
+
+#requests_toolbelt.adapters.appengine.monkeypatch()
+
+temp_spaces = """<ol type="1">
+	% if (list[0]["type"] == "FLOOR"):
+		Floors 	<br><br> 
+	%end
+	% for a in list:
+		% if (a["name"] == ""): 
+			<li><a href="http://localhost:8080/0/space/{{a["id"]}}"> {{a["id"]}} </a></li>
+		% else:
+			<li><a href="http://localhost:8080/0/space/{{a["id"]}}"> {{a["name"]}} </a></li>
+		%end
+	% end
+</ol>"""
+
 index = '''
     	Welcome to Room Management
 
@@ -7,37 +26,62 @@ index = '''
     	</form>
     '''
 
-index2 = '''
-	<script type="text/javascript" src="{{ get_url('static', filename='main.js') }}"></script>
-	Welcome to Room Management
-	<form action="javascript:;" onsubmit="login(this)">
-		Username: <input name="username" type="text" /><br><br>
-		<input value="Login" type="submit" />
-	</form>
-	<div id="errorDiv" style="color:red" />
-'''
-
-temp_spaces = """<ol type="1">
-	% if (list[0]["type"] == "FLOOR"):
-		Floors 	<br><br>
-	%end
-	% for a in list:
-		% if (a["name"] == ""):
-			<li><a href="http://localhost:8080/0/space/{{a["id"]}}"> {{a["id"]}} </a></li>
-		% else:
-			<li><a href="http://localhost:8080/0/space/{{a["id"]}}"> {{a["name"]}} </a></li>
-		%end
-	% end
-</ol>"""
-
+#falta verificar se pode fazer check out ou nao
+#a mesma funcao JS repetida alterar isto no ficheiro
 logged_in = """
 
 		<br><a href="http://localhost:8080/user/{{uid}}/rooms">List Available Rooms</a>
 
-    	<br><br>You currently are in room <button type="button" onclick=checkoutuser()> Check out </button>
+    	<br><br>You currently are in room <button type="button" onclick=checkoutuser({{uid}})> Check out </button> 
+    	<div id='provideButton'>
+<script>
+function checkoutuser(uid) {
 
+    var url = 'http://localhost:8080/api/checkout';
+    var userid = {uid: uid}
 
+    var xmlObj = new XMLHttpRequest();
+    xmlObj.open('POST', url, true);
+    xmlObj.setRequestHeader("Content-type", "application/json");
+    xmlObj.onload = function (e) {
+        if (xmlObj.readyState === 4) {
+            if (xmlObj.status === 200) {
+            	var exemplo=JSON.parse(xmlObj.responseText)
+            	if (exemplo["state"]==0){
+            		alert("you are not checked in")
+            	}else{
+            		alert("you were checked out")
+            	}
+
+                document.getElementById('provideButton').disabled = true;
+            } else {
+                document.getElementById('provideButton').innerHTML = 'Something went wrong:' + xmlObj.statusText;
+                console.error(xmlObj);
+            }
+        }
+    };
+    xmlObj.onprogress = function (e) {
+        document.getElementById('provideButton').innerHTML += '.'
+    }
+    xmlObj.onerror = 
+    function (e) {
+        document.getElementById('provideButton').innerHTML = 'Something went wrong:' + e.statusText;
+        console.error(e);
+    };
+
+    xmlObj.send(JSON.stringify(userid));
+}
+</script>
 """
+
+
+"""function checkoutuser() {
+	var xhttp2 = new XMLHttpRequest();
+	xhttp2.open("POST", "http://localhost:8080/api/checkout", true);
+	xhttp2.send("uid={{uid}}");
+	alert('logging out')
+}"""
+
 """
 <script>
 function checkoutuser(idr) {
@@ -48,7 +92,6 @@ function checkoutuser(idr) {
 }
 </script>
 """
-
 temp_failed_login="""
 		The username - {{username}} is already in use please choose another one
 
@@ -57,29 +100,146 @@ temp_failed_login="""
 
 temp = """<ol type="1">
 
-	% for a,b in list["rooms"].items():
-		<li> {{a}} - {{b}} <button type="button" onclick=checkinuser({{a}})> Check in </button> </li>
+	% for id_sala,name in list["rooms"].items():
+		<li> {{id_sala}} - {{name}} <button id= "{{id_sala}}" type="button" onclick=checkinuser({{list["id"]}},{{id_sala}})> Check in </button>
+		<button type="button" onclick=listusers({{id_sala}})> Show users </button></li>
 	% end
+
+	//fazer div para mostrar template
 </ol>
 
 <script>
-function checkinuser(idr) {
+function listusers(id_sala){
+	alert("lista de utilzadores logados");
+	/*var url = 'http://localhost:8080/api/listusers/'+id_sala;
+
+	//window.location.href= url
+
+
 	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "http://localhost:8080/api/checkin", true);
-	xhttp.send("uid={{list["id"]}}&roomid="+idr);
-	alert(idr)
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();*/
+
+    var url = 'http://localhost:8080/api/listusers/'+id_sala;
+
+    var xmlObj = new XMLHttpRequest();
+    xmlObj.open('GET', url, true);
+    xmlObj.setRequestHeader("Content-type", "application/json");
+    xmlObj.onload = function (e) {
+        if (xmlObj.readyState === 4) {
+            if (xmlObj.status === 200) {
+            	var exemplo=JSON.parse(xmlObj.responseText)
+            	if (exemplo["state"]== 0){
+            		alert("Nao esta ninguem na sala");
+            	}else{
+            		alert("Esta alguem na sala");
+            		alert(JSON.stringify(exemplo["users"]));
+            	}
+            	//window.location.href= url
+                //document.getElementById(id_sala).disabled = true;
+            } else {
+                document.getElementById(id_sala).innerHTML = 'Something went wrong:' + xmlObj.statusText;
+                console.error(xmlObj);
+            }
+        }
+    };
+    xmlObj.onprogress = function (e) {
+        document.getElementById(id_sala).innerHTML += '.'
+    }
+    xmlObj.onerror = function (e) {
+        document.getElementById(id_sala).innerHTML = 'Something went wrong:' + e.statusText;
+        console.error(e);
+    };
+    xmlObj.send();
+
+}
+
+function checkoutuser(uid) { 
+
+    var url = 'http://localhost:8080/api/checkout';
+    var userid = {uid: uid}
+
+    var xmlObj = new XMLHttpRequest();
+    xmlObj.open('POST', url, true);
+    xmlObj.setRequestHeader("Content-type", "application/json");
+    xmlObj.onload = function (e) {
+        if (xmlObj.readyState === 4) {
+            if (xmlObj.status === 200) {
+            	var exemplo=JSON.parse(xmlObj.responseText)
+            	if (exemplo["state"]==0){
+            		alert("you are not checked in")
+            	}else{
+            		alert("you were checked out")
+            	}
+
+                document.getElementById('provideButton').disabled = true;
+            } else {
+                document.getElementById('provideButton').innerHTML = 'Something went wrong:' + xmlObj.statusText;
+                console.error(xmlObj);
+            }
+        }
+    };
+   /* xmlObj.onprogress = function (e) {
+        document.getElementById('provideButton').innerHTML += '.'
+    }*/
+    xmlObj.onerror = function (e) {
+        document.getElementById('provideButton').innerHTML = 'Something went wrong:' + e.statusText;
+        console.error(e);
+    };
+
+    xmlObj.send(JSON.stringify(userid));
+}
+
+function checkinuser(usid ,idr) {
+
+    var url = 'http://localhost:8080/api/checkin';
+    var data = {uid: usid, roomid: idr};
+
+    var xmlObj = new XMLHttpRequest();
+    xmlObj.open('POST', url, true);
+    xmlObj.setRequestHeader("Content-type", "application/json");
+    xmlObj.onload = function (e) {
+        if (xmlObj.readyState === 4) {
+            if (xmlObj.status === 200) {
+            	var exemplo=JSON.parse(xmlObj.responseText)
+            	if (exemplo["state"]==1){
+            		alert("you are now checked in")
+            	}else{
+            		alert("You are already in a room")
+            		checkoutuser(usid)
+            		alert("You are in a new room now")
+            		checkinuser(usid, idr)
+            	}
+                //document.getElementById(idr).disabled = true;
+            } else {
+                document.getElementById(idr).innerHTML = 'Something went wrong:' + xmlObj.statusText;
+                console.error(xmlObj);
+            }
+        }
+    };
+    /*xmlObj.onprogress = function (e) {
+        document.getElementById(idr).innerHTML += '.'
+    }*/
+    xmlObj.onerror = function (e) {
+        document.getElementById(idr).innerHTML = 'Something went wrong:' + e.statusText;
+        console.error(e);
+    };
+
+    xmlObj.send(JSON.stringify(data));
 }
 </script>
 """
+
 
 temp_provide = """<ol>
 	Type : {{list["type"]}} <br>
 	Name : {{list["name"]}} <br>
 	Campus : {{list["parentSpace"]["topLevelSpace"]["name"]}}<br> <br>
 
-	<button id="provideButton" onclick="provideRoom({{list["id"]}}, \'{{list["name"]}}\')">Provide Room</button>
+	<button onclick="myFunction()">Provide Room</button> 
 
-	<script type="text/javascript" src="{{ get_url('static', filename='main.js') }}"></script>
+	<script language="Javascript" type="text/javascript" src="{{ get_url('static', filename='js/javascript.js') }}" /> 
 </ol>"""
 
 temp_utilizadores= """
